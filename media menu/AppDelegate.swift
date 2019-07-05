9ccd4a345281f7ea70d193257b69dedb908b5fb4
Michael Newbould <michael.newbould@intix.eu>
2019-07-05 18:07:02 +0200

    Initial Commit

---
+++ media menu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  media menu
+//
+//  Created by Michael Newbould on 05/07/2019.
+//  Copyright © 2019 Michael Newbould. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ media menu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  media menu
+//
+//  Created by Michael Newbould on 05/07/2019.
+//  Copyright © 2019 Michael Newbould. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
